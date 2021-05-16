d9afb00de2794bcbb35acf1356568f6c39cc72cb
yufeng97 <47183963+yufeng97@users.noreply.github.com>
2021-05-16 22:59:16 +0800

    change to class

--- crawl/twitter.py
+++ crawl/twitter.py
@@ -12,18 +12,67 @@ from tweepy.streaming import StreamListener
 # access_token = '1381264077268283393-otuXk0e8EVuw5BnrYbKpEuVDButxQI'  
 # access_token_secret = '9HMXSW6FzC24nlA1sccTaF8Wwypdh2KHbpxz2GT6jSB94'
 
-consumer_key = 'thXKO36EQXVLHLyCI9OmtDYxz'  
-consumer_secret = 'sejP7Ejxgwj7CeJ7v6Cq0Xgkjr6wlX7nqAGN7JI4EFrBT9aBYMsM'  
-access_token = '1388443056366522373-hF6fQq4h6v8Y6NYb8N0mg819881M5w'  
-access_token_secret = '55VHShxJhL8r8ZxVRcOsyUvcqjDc3mDSZAPqgerpRcTjZ'  
+# consumer_key = 'T08RKyC74lJTtOeozaBMSjbs9'  
+# consumer_secret = 'nBbdYRK7BmsoD5h23l9OT4ICQ2CABnmcCJAJunZtFtv556bYzL'  
+# access_token = '1388437713536229380-rUyKUTYcwz887Hw0AdsLbhZtfapxeJ'  
+# access_token_secret = 'bmZSM7APPq9MFbNZqiBsMkE9nk3rpZVU07qDzNfePg6T2'  
 
+
+consumer_key = 'xEq8Jp0JQ2dGV83zckKcmM7OD'  
+consumer_secret = 'WTxu63zuHlq7tkHr7EQgyZEBQ7bSlr42SmTcfBUoEbvvG498Lx'  
+access_token = '1387770902490849283-Xl1JItql6wHiYLt4VUY86ztzOtJSTn'  
+access_token_secret = 'v06774P9GqModBseAlKIWVmK9a6asKSbkc8fk7mEFhW1g'
 
 keywords = [
     "covid",  "virus", "corona", "outbreak", "covid-19", "quarantine"
 ]
+keyword = f'({" OR ".join(keywords)})'
 DB_AUTH = {"ADDRESS": "172.26.130.120", "PORT": "5984", "COUCHDB_USER": "admin", "COUCHDB_PASSWORD": "group27"}
 BATCH_SIZE = 150
 
+
+class Crawler:
+    def __init__(self, api, db, suburbs, tweet_count: int = 1000):
+        self.db = db
+        self.api = api
+        self.suburbs = suburbs
+        self.tweet_count = tweet_count
+        self.tweets = []
+
+    def crawl(self, query, suburb):
+        for tweet in tweepy.Cursor(self.api.search_30_day, query=query, environment_name='90024').items(self.tweet_count):
+            temp = tweet._json
+            if temp is not None:
+                temp['suburb'] = suburb
+                self.tweets.append(temp)
+            if len(self.tweets) >= BATCH_SIZE:
+                self.db.insert_tweets(self.tweets)
+                self.tweets.clear()
+
+    def run(self):
+        for suburb, coordinates in self.suburbs.items():
+            lat, lon = coordinates
+            geocode = f'[{lon} {lat} 4km]'
+            print(geocode)
+            query = f'{keyword} point_radius:{geocode}'
+            try:
+                self.crawl(query, suburb)
+            except TweepError as e:
+                print(e)
+                if e.response.status_code == 429:
+                    print("Waiting on limit for 1 min")
+                    time.sleep(60)
+                    try:
+                        self.crawl(query, suburb)
+                    except Exception as e:
+                        print("retry failed")
+                        continue
+                else:
+                    time.sleep(10)
+                continue
+        if len(self.tweets) > 0:
+            self.db.insert_tweets(self.tweets)
+        print("Finished")
 
 
 def get_api():
@@ -42,7 +91,6 @@ if __name__ == '__main__':
 
     tweet_count = args.count
     dbname = args.dbname
-    keyword = f'({" OR ".join(keywords)})'
 
 
     with open("melb_coordinates.json", "r") as f:
@@ -55,29 +103,5 @@ if __name__ == '__main__':
 
     api = get_api()
 
-    tweets = []
-    for suburb, coordinates in suburbs.items():
-        lat, lon = coordinates
-        geocode = f'[{lon} {lat} 3km]'
-        print(geocode)
-        query = f'{keyword} point_radius:{geocode}'
-        try:
-            for tweet in tweepy.Cursor(api.search_30_day, query=query, environment_name='dev').items(1000):
-                temp = tweet._json
-                if temp is not None:
-                    temp['suburb'] = suburb
-                    tweets.append(temp)
-                if len(tweets) >= BATCH_SIZE:
-                    db.insert_tweets(tweets)
-                    tweets.clear()
-        except TweepError as e:
-            print(e)
-            if e.response.status_code == 429:
-                print("Waiting on limit for 15 mins")
-                time.sleep(15 * 60 + 1)
-            else:
-                time.sleep(10)
-            continue
-    if len(tweets) > 0:
-        db.insert_tweets(tweets)
-    print("Finished")
+    crawler = Crawler(api, db, suburbs, tweet_count)
+    crawler.run()
