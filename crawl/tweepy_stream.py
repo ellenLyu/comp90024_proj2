284c698facc3c408abe2e91416ad54914103cc48
yufeng97 <47183963+yufeng97@users.noreply.github.com>
2021-05-25 23:49:08 +0800

    update bbox

--- crawl/tweepy_stream.py
+++ crawl/tweepy_stream.py
@@ -17,7 +17,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
 dbname = "tweets"
-melb_bbox = [144.33363404800002, -38.50298801599996, 145.8784120140001, -37.17509899299995]
+melb_bbox = [112.28, -44.36, 155.23, -10.37]
 DB_AUTH = {"ADDRESS": "172.26.131.194", "PORT": "5984", "COUCHDB_USER": "admin", "COUCHDB_PASSWORD": "group27"}
 url = 'http://{0}:{1}@{2}:{3}/'.format(DB_AUTH["COUCHDB_USER"], DB_AUTH["COUCHDB_PASSWORD"],
                                         DB_AUTH["ADDRESS"], DB_AUTH["PORT"])
@@ -26,8 +26,9 @@ db = database.Connection(url=url, database_name=dbname)
 
 class CustomStreamListener(tweepy.StreamListener):
     def on_data(self, raw_data):
-        print(raw_data["text"])
-        db.insert_tweets([raw_data])
+        data = json.loads(raw_data)
+        print(data["text"])
+        db.insert_tweets([data])
 
     def on_error(self, status_code):
         #print(status)
